7dbb5d741a08211fecf1e946a8ca309b40295fde
Reis Souza <reis.souza@outlook.com.br>
2023-01-01 19:42:04 -0300

    feat: :memo: implmentation docmentation swagger

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,19 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 
 import { router } from 'routes';
+import swaggerUi from 'swagger-ui-express';
 
 import { AppError } from '@shared/Util/AppError/AppError';
+
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
